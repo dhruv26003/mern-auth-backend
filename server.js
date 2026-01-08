689baac688c6bc6e91e9106e15ca1feb75a7c824
dhruv263 <dhruvchavda3232@gmail.com>
2026-01-08 16:29:47 +0530

    Fix CORS for production

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const connectDB = require("./config/db");
 const app = express();
 connectDB();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
 app.use(express.json());
 
 app.use("/api", require("./routes/authRoutes"));
